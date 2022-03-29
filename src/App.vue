d88b4869d8f3d8fa9fa3325337eb7289d9added4
独行 <jsw0325@163.com>
2022-03-29 22:25:03 +0800

    feat: 更改默认语言

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+</script>
+
 <template>
-  <router-view></router-view>
+  <a-config-provider :locale="zhCN">
+    <router-view />
+  </a-config-provider>
 </template>
